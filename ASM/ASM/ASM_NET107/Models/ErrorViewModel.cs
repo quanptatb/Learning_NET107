c9a45b9d261412002146963d3ebe79f9dce66c05
quanptatb <quanptatb01758@gmail.com>
2025-12-11 23:10:11 +0700

    99% asm

--- ASM/ASM/ASM_NET107/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASM_NET107.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ ASM/ASM/ASM_NET107_TB01758/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using System.Drawing;
+using System.Security.Cryptography.Xml;
+using System.Security.Principal;
+
+namespace ASM_NET107_TB01758.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
