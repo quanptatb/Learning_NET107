784d3719758f27a31426dc11a6934eb361189f1c
quanptatb <quanptatb01758@gmail.com>
2025-12-11 23:37:25 +0700

    99% asm

---
+++ ASM/ASM/ASM_NET107_TB01758/DAL/DatabaseHelper.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ASM_NET107_TB01758.DAL
+{
+    public class DatabaseHelper
+    {
+        private readonly string _connStr;
+
+        public DatabaseHelper(IConfiguration config)
+        {
+            _connStr = config.GetConnectionString("DefaultConnection");
+        }
+
+        public DataTable GetRecords(string query, params SqlParameter[] parameters)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    if (parameters != null) cmd.Parameters.AddRange(parameters);
+                    conn.Open();
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        da.Fill(dt);
+                    }
+                }
+            }
+            return dt;
+        }
+
+        public int ExecuteNonQuery(string query, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    if (parameters != null) cmd.Parameters.AddRange(parameters);
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public object ExecuteScalar(string query, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    if (parameters != null) cmd.Parameters.AddRange(parameters);
+                    conn.Open();
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+    }
+}
